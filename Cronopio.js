9be8020caacbcec38f60c65d8a60475f7bb7a123
naranjujeli <lucadive@gmail.com>
2023-10-22 23:36:31 -0300

    Clase Vector y Cronopio

--- Cronopio.js
+++ Cronopio.js
@@ -1,161 +1,95 @@
 class Cronopio {
-    constructor(position, initialLifeDuration) {
-        this.position = position;
+    constructor(
+        position, 
+        diameter,
+        a,
+        b,
+        t,
+        initialLifeDuration=100,
+        windowSize={width: 1500, height: 800}
+    ) {
+        this.position = new Vector(position.x, position.y);
+        this.diameter = diameter;
+        this.a = a;
+        this.b = b;
+        this.t = t;
         this.initialLifeDuration = initialLifeDuration;
         this.life = this.initialLifeDuration;
+        this.windowSize = windowSize;
+        
+        this.alive = true;
+        this.velocity = new Vector(0, 0);
+        this.duration = 0;
+        this.tCounter = 0;
     }
+
+    ableToEat(food) {
+        const differenceInX = Math.abs(food.x-this.position.x);
+        const differenceInY = Math.abs(food.y-this.position.y);
+        const difference = Math.sqrt(Math.pow(differenceInX, 2) + Math.pow(differenceInY, 2));
+        return (difference < this.diameter);
+    }
+
+    time() {
+        this.life--;
+        this.duration++;
+        if (this.life === 0) {
+            this.alive = false;
+            return;
+        }
+        this.tCounter++;
+        if (this.tCounter % this.t === 0) {
+            this.tCounter = 0;
+            this.update();
+        }
+        this.move();
+    }
+
+    update() {
+        this.changeVelocity();
+        this.updateMovement();
+        this.bounce();
+    }
+
+    move() {
+        this.position = this.position.add(this.velocity);
+    }
+    
+    changeVelocity() {
+        this.velocity = this.calculateNewVelocity();
+    }
+
+    updateMovement() {
+        const angle = Math.random() * 10 - 5;
+        this.velocity.changeDirection(angle);
+    }
+
+    calculateNewVelocity() {
+        const p = (x) => -25*(Math.pow(x, 2))+this.b*x+this.a;
+        const f = (x) => 3/(1+Math.pow((Math.E)(-p(x)), 2));
+        x = 2*Math.random()-1;
+
+        const newVelocityMagnitude = f(x);
+        const newVelocity = new Vector(1, 1);
+        newVelocity.setMagnitude(newVelocityMagnitude);
+        return newVelocity;
+    }
+
+    bounce() {
+        // TODO cambiar esto mas tarde
+        const frame = 100;
+        if (this.position.x + this.diameter > this.windowSize.width)
+            this.position.x = frame;
+        if (this.position.x - this.diameter < 0)
+            this.position.x = this.windowSize.width-frame;
+        if (this.position.y + this.diameter > this.windowSize.height)
+            this.position.y = frame;
+        if (this.position.y - this.diameter < 0)
+            this.position.y = this.windowSize.height-frame;
+    }
+
+    eat() {
+        this.life += this.initialLifeDuration / 10;
+    }
+
 }
-
-// CÓDIGO DE FICHITA, BORRAR MÁS TARDE
-// import uuid
-// import math
-// from random import randint, random, choice
-// from vector import Vector
-
-// class Cronopio:
-
-//     def __init__(self, **kargs):
-
-//         self.__id = uuid.uuid4()
-//         self.__inicial_life_duration = 1000
-//         self.__life = self.__inicial_life_duration
-
-//         self.__duration = 0
-
-//         self.__frame = kargs['frame']
-
-//         pos_x = randint(self.__frame, kargs['window_size'].x - self.__frame)
-//         pos_y = randint(self.__frame, kargs['window_size'].y - self.__frame)
-//         self.__pos = Vector(pos_x, pos_y)
-
-//         self.__a = kargs['a']
-//         self.__b = kargs['b']
-//         self.__t = kargs['t']
-
-//         self.__vel = self.__new_vel()
-        
-//         self.__t_counter = 0
-
-//         if not kargs['d']:
-//             self.__d = self.__get_diameter()
-//         else:
-//             self.__d = kargs['d']
-
-//         self.__alive = True
-
-//     def __eq__(self, other):
-//         return self.__id == other.id
-
-//     def fitness(self):
-//         return self.__duration
-    
-//     def mutate(self):
-        
-//         self.__a = randint(-1, 1)
-//         self.__b = randint(-1, 1)
-//         self.__t += choice([-5, 5])
-//         self.__d += 2
-
-//     def time(self, window_size):
-
-//         self.__life -= 1
-//         self.__duration += 1
-//         if self.__life == 0:
-//             self.__alive = False
-//             return
-//         self.__t_counter += 1
-//         if self.__t_counter % self.__t == 0:
-//             self.__t_counter = 0
-//             self.__update(window_size)   
-//         self.__move()
-
-//     def __update(self, window_size):
-//         self.__change_velocity()
-//         self.__update_movement()
-//         self.__bounce(window_size)
-
-//     def __change_velocity(self):
-//         self.__vel = self.__new_vel()
-    
-//     def __new_vel(self):
-
-//         p = lambda x: -25*(x**2) + self.__b*x + self.__a
-//         f = lambda x: 3/(1+(math.e)**(-p(x)))
-//         x = 2*random() - 1
-
-//         new_vel_mag = f(x)
-//         new_vel = Vector(1, 1)
-//         new_vel.set_mag(new_vel_mag)
-//         return new_vel
-    
-//     def __update_movement(self):
-//         angle = randint(-5, 5)
-//         self.__vel.change_dir(angle)
-
-//     def __bounce(self, window_size):
-
-//         if self.__pos.x + self.__d > window_size.x: self.__pos.set_x(self.__frame)
-//         if self.__pos.x - self.__d < 0: self.__pos.set_x(window_size.x-self.__frame)
-//         if self.__pos.y + self.__d > window_size.y: self.__pos.set_y(self.__frame)
-//         if self.__pos.y - self.__d < 0: self.__pos.set_y(window_size.y-self.__frame)
-
-//     def __move(self):
-//         self.__pos += self.__vel
-
-//     def eat(self):
-//         self.__life += self.__inicial_life_duration/10
-
-//     def able_to_eat(self, food):
-        
-//         dif_x = abs(food.x - self.__pos.x)
-//         dif_y = abs(food.y - self.__pos.y)
-//         distance = math.sqrt(dif_x**2 + dif_y**2)
-
-//         return distance < (self.__d)
-    
-//     def __get_diameter(self):
-        
-//         d = lambda y: max(15 - abs(12*y - 3*self.__b), 1)
-//         y = 2*random() - 1
-
-//         return d(y)*1
-    
-//     @property
-//     def alive(self):
-//         return self.__alive
-    
-//     @property
-//     def duration(self):
-//         return self.__duration
-
-//     @property
-//     def id(self):
-//         return self.__id
-    
-//     @property
-//     def a(self):
-//         return self.__a
-    
-//     @property
-//     def b(self):
-//         return self.__b
-    
-//     @property
-//     def t(self):
-//         return self.__t
-    
-//     @property
-//     def life(self):
-//         return self.__life
-    
-//     @property
-//     def pos(self):
-//         return (self.__pos.x, self.__pos.y)
-    
-//     @property
-//     def vel(self):
-//         return self.__vel
-    
-//     @property
-//     def diameter(self):
-//         return self.__d
---
+++ Vector.js
@@ -0,0 +1,48 @@
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    getMagnitude() {
+        return (Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
+    }
+
+    getDirection(mode="DEGREES") {
+        if (mode === "DEGREES") {
+            return (Math.atan(this.y / this.x));
+        } else if (mode === "RADIANS") {
+            return (Math.atan(this.y / this.x) * Math.PI/180);
+        }
+    }
+    
+    setMagnitude(newMagnitude) {
+        this.setDirection(this.getDirection());
+        this.scale(newMagnitude);
+    }
+
+    setDirection(direction) {
+        this.x = this.getDirection()*Math.cos(direction);
+        this.y = this.getDirection()*Math.sin(direction);
+    }
+    
+    changeDirection(off) {
+        this.setDirection(this.getDirection() + off);
+    }
+
+    add(otherVector) {
+        return (new Vector(this.x + otherVector.x, this.y + otherVector.y));
+    }
+
+    scale(s) {
+        this.x *= s; 
+        this.y *= s;
+    }
+
+    normalize() {
+        const direction = this.getDirection();
+        const newVector = new Vector(1, 1);
+        newVector.setDirection(direction);
+        return newVector;
+    }
+}
